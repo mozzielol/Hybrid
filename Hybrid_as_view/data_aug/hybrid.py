cff520608b67caaa1da45dc59a95e52d5dd5ac2d
Yifei <yifeioffice@gmail.com>
2021-05-11 21:02:48 -0700

    Add function for generating a list of mixture of hybrid/original images and similarity matrix

--- Hybrid_as_view/data_aug/hybrid.py
+++ Hybrid_as_view/data_aug/hybrid.py
@@ -34,3 +34,61 @@ def get_hybrid_images(image_batch, origin=False, kernel=(9, 9)):
         image_batch) else hybrid_images
 
     return hybrid_images
+
+
+def generate_pairs_with_hybrid_images(seed_images, kernel=(9, 9), weights=(0.5, 0.5)):
+    """
+    Generate a batch of mixture of hybrid and original images, as well as the corresponding similarity matrix
+    :param seed_images: a list of original seed images (N x D)
+    :param kernel: kernel size for generating hybrid images
+    :param weights: weights of hybrid and original images in the generated image list
+    :return: a list of generated images (2N x D), as well as the similarity matrix (2N x 2N)
+    """
+    weights = (abs(w) / sum(weights) for w in weights)
+    num_seed = len(seed_images)
+    num_pure = round(2 * num_seed * weights[0])
+    num_hybrid = 2 * num_seed - num_pure
+
+    # Compute list of indices, each element corresponds to the seed index/indices of a output image
+    pure_indices = [(idx,) for idx in np.random.choice(num_seed, num_pure, replace=True)]
+    hybrid_indices = [tuple(np.random.choice(num_seed, 2, replace=False)) for i in range(num_hybrid)]
+    composition_indices = pure_indices + hybrid_indices
+    np.random.shuffle(composition_indices)
+
+    # Generate output images
+    generated_images = []
+    for indices in composition_indices:
+        if len(indices) > 1:
+            hybrid_image = compose_hybrid_image(seed_images[indices[0]], seed_images[indices[1]], kernel=kernel)
+            generated_images.append(hybrid_image)
+        else:
+            generated_images.append(seed_images[indices[0]])
+
+    # Compute similarity matrix
+    def measure_similarity(indices1, indices2):
+        if len(indices1) == len(indices2):
+            if indices1 == indices2:
+                return 1  # (A), (A)
+            else:
+                if len(indices1) == 1:
+                    return 0  # (A), (B)
+                else:
+                    if indices1[0] == indices2[0] or indices1[1] == indices2[1]:
+                        return 0.3  # (A, B), (A, C)
+                    elif indices1[0] == indices2[1]:
+                        return 0.8 if indices1[1] == indices2[0] else 0.2  # (A, B), (B, A) - .8; (A, B), (C, A) - .2
+                    elif indices1[1] == indices2[0]:
+                        return 0.2  # (A, B), (B, C)
+                    else:
+                        return 0  # (A, B), (C, D)
+        else:
+            _h_indices, _p_indices = (indices1, indices2) if len(indices1) > len(indices2) else (indices2, indices1)
+            return 0.5 if _p_indices[0] in _h_indices else 0  # (A, B), A - 0.5; (A, B), C - 0
+
+    similarity_matrix = np.zeros((2 * num_seed,) * 2)
+    for i in range(num_seed):
+        for j in range(i):
+            similarity_matrix[i, j] = measure_similarity(composition_indices[i], composition_indices[j])
+            similarity_matrix[j, i] = measure_similarity(composition_indices[i], composition_indices[j])
+
+    return generated_images, similarity_matrix
