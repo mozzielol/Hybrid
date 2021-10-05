b72b40073978c871e3e696f137e4acd18401a0cd
mozzielol <mozzielol@hotmail.com>
2021-10-05 20:40:33 +0100

    add loss

--- partial_order/loss/order_loss.py
+++ partial_order/loss/order_loss.py
@@ -6,11 +6,11 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class Order_loss(torch.nn.Module):
 
-    def __init__(self, dim, delta, use_cosine_similarity):
+    def __init__(self, dim, delta, use_cosine_similarity, loss_function):
         super(Order_loss, self).__init__()
         self._cosine_similarity = torch.nn.CosineSimilarity(dim=-1)
         self.measure_similarity = self._get_similarity_function(use_cosine_similarity)
-        self.criterion = torch.nn.BCELoss(reduction='sum')
+        self.criterion = torch.nn.BCELoss(reduction='sum') if loss_function == 'bce' else torch.nn.MSELoss(reduction='sum')
         self.delta = delta
         self.use_cosine_similarity = use_cosine_similarity
         self.projector = torch.nn.Linear(dim, dim, bias=False).to(device)
