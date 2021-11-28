Fix order loss computation for cosine distance

partial_order/loss/order_loss.py
@@ -13,6 +13,7 @@ class Order_loss(torch.nn.Module):
         self.criterion = torch.nn.BCELoss(reduction='sum') if loss_function == 'bce' else torch.nn.MSELoss(reduction='sum')
         self.delta = delta
         self.use_cosine_similarity = use_cosine_similarity
+        self.loss_function = loss_function
         self.projector = torch.nn.Linear(dim, dim, bias=False).to(device)
         self.mahalanobis_cov = torch.nn.Parameter(torch.rand((dim, dim)), requires_grad=True)
 
@@ -64,20 +65,27 @@ class Order_loss(torch.nn.Module):
         :param z_anchor: anchor image
         :return:
         """
-        if single_pair:
-            s1 = torch.diag(self.measure_similarity(zis, z_anchor)) if self.use_cosine_similarity else self.measure_similarity(zis, z_anchor)
-            s2 = torch.diag(self.measure_similarity(zjs, z_anchor)) if self.use_cosine_similarity else self.measure_similarity(zjs, z_anchor)
+        s1 = torch.diag(self.measure_similarity(zis, z_anchor)) if self.use_cosine_similarity \
+            else self.measure_similarity(zis, z_anchor)
 
+        if single_pair:
+            s2 = torch.diag(self.measure_similarity(zjs, z_anchor)) if self.use_cosine_similarity \
+                else self.measure_similarity(zjs, z_anchor)
+            differences = torch.clamp(s2 - s1 + self.delta, min=0)
         else:
-            s1 = self.measure_similarity(zis, z_anchor)
-            s2 = []
-            for count in range(1, len(z_anchor) - 1):
-                s2.append(self.measure_similarity(zjs, torch.roll(z_anchor, count, 0)))
-            s2 = torch.stack(s2)
-            # loss = -torch.sum(torch.log(torch.mean(torch.clamp(s2 - s1 + self.delta, min=1e-5, max=1.), dim=-1)))
-        differences = torch.clamp(s2 - s1 + self.delta, min=0)
-        #      differences = differences - differences.min()
-        #      differences = differences / differences.max()
+            if self.use_cosine_similarity:
+                s2 = self.measure_similarity(zjs, z_anchor)
+            else:
+                s2 = []
+                for count in range(1, len(z_anchor) - 1):
+                    s2.append(self.measure_similarity(zjs, torch.roll(z_anchor, count, 0)))
+                s2 = torch.stack(s2)
+            differences = torch.clamp(s2 - s1.reshape(-1, 1) + self.delta, min=0)
+
+        if not self.use_cosine_similarity and self.loss_function.lower() == 'bce':  # BCE requires inputs in [0, 1]
+             differences = differences - differences.min()
+             differences = differences / differences.max()
+
         loss = self.criterion(differences, torch.zeros_like(differences))
         return loss
 